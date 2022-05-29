843d632035a5a9b7635b2759eb504584bc8ad072
RafalJanko <83403007+RafalJanko@users.noreply.github.com>
2022-05-29 19:02:52 +0200

    Update views.py

--- Books/views.py
+++ Books/views.py
@@ -64,11 +64,11 @@ class OneBookView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def patch(self, request, pk):
-        book = self.get_object(pk)
+        book = Books.objects.get(id=pk)
         serializer = BooksSerializer(book, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST, data="Wrong parameters")
 
 
